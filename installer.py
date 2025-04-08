26a037c46917aa54690963c2272548d7cc24f93d
oliik2013 <131269027+oliik2013@users.noreply.github.com>
2025-04-08 14:48:36 +0000

    smh

--- installer.py
+++ installer.py
@@ -1,5 +1,7 @@
 import platform
 
-if platform.system() not "Windows":
+if not platform.system() == "Windows":
   print("The installer isnt supported on your os yet! To get the service, install the manual way")
   exit()
+
+print("hi")
