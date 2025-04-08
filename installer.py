4b80de3f99d59792f3435b765717d03b6915c322
oliik2013 <131269027+oliik2013@users.noreply.github.com>
2025-04-08 15:09:43 +0000

    admin

--- installer.py
+++ installer.py
@@ -1,7 +1,17 @@
 import platform
+import ctypes, sys
 
 if not platform.system() == "Windows":
   print("The installer isnt supported on your os yet! To get the service, install the manual way")
   exit()
 
-print("hi")
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+    
+if is_admin():
+    print("yoo")
+else:
+    ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
